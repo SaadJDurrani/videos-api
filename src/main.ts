1b44ecc0111165d8dbd653002eed0ff019b50473
SaadJDurrani <saadjandurrani@gmail.com>
2024-07-31 14:19:25 +0500

    get post

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,24 @@ import * as express from "express";
 const PORT = 3000;
 const app = express();
 
+app.use(express.json());
+app.listen(PORT, () => {
+  console.log(`App listening at http://127.0.0.1:${PORT}`);
+});
+
 function getController(_req: express.Request, res: express.Response) {
   res.send("Hello World");
 }
 app.get("/", getController);
+app.get("/user/:name", (req, res) => {
+  // @ts-ignore
+  res.send({ path: req.params.name, query: req.query.qp });
+});
 app.get("/video", (_req, res) => {
   res.send({ videoURL: "hdashdhsadh" });
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening at http://127.0.0.1:${PORT}`);
+app.post("/video", (req, res) => {
+  console.log(req.body);
+  res.send({ videoURL: "hdashdhsadh", payload: req.body });
 });
